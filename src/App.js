e23d0491a9bb6bb0243cc364f4f4c75861e21b63
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-01-10 15:47:34 +0900

    implement componete using multiple useStates.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,29 @@
-import { useState } from "react";
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = props => {
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-
-  const reset = () => setCount(0)
-  const multiply = () => setCount(count * 2)
-  const only3devide = () => setCount(previousCount => {
-    // if (count % 3 === 0) {
-    //   setCount(count / 3)
-    // } else {
-
-    // }
-    return previousCount % 3 === 0 ? previousCount / 3 : previousCount
-  })
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
   return (
     <>
-      <div>
-        count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={multiply}>x2</button>
-        <button onClick={only3devide}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>現在の{name}は{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}></input>
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
