4d15bd364bd93c25520607a3378e539d4ab85cc7
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-01-10 15:07:07 +0900

    this is my first react hooks app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useState } from "react";
 import React from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+
+  const reset = () => setCount(0)
+  const multiply = () => setCount(count * 2)
+  const only3devide = () => setCount(previousCount => {
+    // if (count % 3 === 0) {
+    //   setCount(count / 3)
+    // } else {
+
+    // }
+    return previousCount % 3 === 0 ? previousCount / 3 : previousCount
+  })
+
   return (
-    <div>
-      This is a template for React App.
-    </div>
+    <>
+      <div>
+        count: {count}
+      </div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={multiply}>x2</button>
+        <button onClick={only3devide}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
   );
 }
 
