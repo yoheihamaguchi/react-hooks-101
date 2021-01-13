351f28f4bfa85886dbc054aee6494be391082cf4
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-01-13 23:21:22 +0900

    replace type names with constants

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
+import { DELETE_EVENT } from '../actions'
 
 const Event = ({ dispatch, event }) => {
     const id = event.id
     const handleClickDeleteButton = () => {
         const result = window.confirm(`イベント(id=${id})を本当に削除してもいいですか？`)
         if (result) dispatch({
-            type: 'DELETE_EVENT',
+            type: DELETE_EVENT,
             id: id,
         })
     }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { CREATE_EVENT, DELETE_EVENT, DELETE_ALL_EVENTS } from '../actions'
 // action = {
 //     type: 'CREATE_EVENT',
 //     title: '2020東京オリンピックのお知らせ',
@@ -35,7 +36,7 @@
 
 const events = (state = [], action) => {
     switch (action.type) {
-        case 'CREATE_EVENT':
+        case CREATE_EVENT:
             const event = {
                 title: action.title,
                 body: action.body,
@@ -44,9 +45,9 @@ const events = (state = [], action) => {
             const id = length === 0 ? 1 : state[length - 1].id + 1
             return [...state, { id, ...event }]
 
-        case 'DELETE_EVENT':
+        case DELETE_EVENT:
             return state.filter(event => event.id !== action.id)
-        case 'DELETE_ALL_EVENTS':
+        case DELETE_ALL_EVENTS:
             return []
         default:
             return state
